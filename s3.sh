a8498c19cb891d9ec06af37be190eccfc9284e1e
sharanushettar644 <98380463+sharanushettar644@users.noreply.github.com>
2022-01-31 12:59:44 +0530

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -1,2 +1,2 @@
 #! /usr/bin/bash
-aws s3api create-bucket --bucket Sharan-jenkins-job --create-bucket-configuration LocationConstraint=eu-west-2
+aws s3api create-bucket --bucket Sharan-jenkins-job --create-bucket-configuration LocationConstraint=us-west-2
