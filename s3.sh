81e7db84ce6ac78985cd6fac3d4d9c2973d5ca15
Ubuntu <ubuntu@ip-172-31-38-103.us-west-2.compute.internal>
2022-01-31 07:19:41 +0000

    s3

--- s3.sh
+++ s3.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/bash
+bucketname=`sharan-s3-from-jenkins`
 function createbucket()
    {
 	       aws s3api  create-bucket --bucket $bucketname --region us-west-2
 	          }
+	  function tagbucket()    {
+		      
+		     aws s3api  put-bucket-tagging --bucket $bucketname --tagging 'TagSet=[{Key=Name,Value="'$bucketname'"}]'
+	     }
+
