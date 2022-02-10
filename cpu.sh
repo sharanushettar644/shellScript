ff5a4df7b4736161279ceafd9afd8e5cfab91e76
Ubuntu <ubuntu@ip-172-31-38-103.us-west-2.compute.internal>
2022-02-10 10:33:11 +0000

    update

--- cpu.sh
+++ cpu.sh
@@ -7,8 +7,7 @@ df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}' "
 b=" top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' "
 
 
-c= "
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello,the msg is from cron to alert u!"}' https://hooks.slack.com/services/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 "
+c="curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello,the msg is from cron to alert u!"}' https://hooks.slack.com/services/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 "
 
 echo $a $b $c
 
